575b343260f48ccad429bddd9714991018e07b6b
jlhiatt <jlhiatt@ldschurch.org>
2018-08-25 22:33:07 -0600

    added an Admin modules with a Users page; Trying to get the tests working.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'fp-root',
@@ -6,4 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+    faCoffee = faCoffee;
 }

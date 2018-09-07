0421821e0b980ca92a019660c1fa3cba79557554
jlhiatt <jlhiatt@ldschurch.org>
2018-09-07 12:33:28 -0600

    fixed the silver & bronze placements.

--- src/app/features/leaderboard/leaderboard.component.ts
+++ src/app/features/leaderboard/leaderboard.component.ts
@@ -45,6 +45,54 @@ const colors = [
     '#C36766',
 ];
 
+const gradientBarChartConfiguration =  {
+    maintainAspectRatio: false,
+    legend: {
+        display: false
+    },
+
+    tooltips: {
+        backgroundColor: '#f5f5f5',
+        titleFontColor: '#333',
+        bodyFontColor: '#666',
+        bodySpacing: 4,
+        xPadding: 12,
+        mode: "nearest",
+        intersect: 0,
+        position: "nearest"
+    },
+    responsive: true,
+    scales:{
+        yAxes: [{
+
+            gridLines: {
+                drawBorder: false,
+                color: 'rgba(29,140,248,0.1)',
+                zeroLineColor: "transparent",
+            },
+            ticks: {
+                suggestedMin: 60,
+                suggestedMax: 120,
+                padding: 20,
+                fontColor: "#9e9e9e"
+            }
+        }],
+
+        xAxes: [{
+
+            gridLines: {
+                drawBorder: false,
+                color: 'rgba(29,140,248,0.1)',
+                zeroLineColor: "transparent",
+            },
+            ticks: {
+                padding: 20,
+                fontColor: "#9e9e9e"
+            }
+        }]
+    }
+};
+
 @Component({
     selector: 'fp-leaderboard',
     templateUrl: './leaderboard.component.html',
@@ -120,7 +168,7 @@ export class LeaderboardComponent implements OnInit {
         let dataSet = {
             label: '',
             data: [],
-            borderWidth: 1,
+            borderWidth: 2,
             borderColor: '',
             backgroundColor: '',
             fill: false
@@ -154,8 +202,54 @@ export class LeaderboardComponent implements OnInit {
 
     }
 
+    /*
+    var ctx = document.getElementById("CountryChart").getContext("2d");
+
+      var gradientStroke = ctx.createLinearGradient(0,230,0,50);
+
+      gradientStroke.addColorStop(1, 'rgba(29,140,248,0.2)');
+      gradientStroke.addColorStop(0.4, 'rgba(29,140,248,0.0)');
+      gradientStroke.addColorStop(0, 'rgba(29,140,248,0)'); //blue colors
+
+
+      var myChart = new Chart(ctx, {
+        type: 'bar',
+        responsive: true,
+        legend: {
+              display: false
+        },
+        data: {
+          labels: ['USA','GER','AUS','UK','RO','BR'],
+          datasets: [{
+            label: "Countries",
+            fill: true,
+            backgroundColor: gradientStroke,
+            hoverBackgroundColor: gradientStroke,
+            borderColor: '#1f8ef1',
+            borderWidth: 2,
+            borderDash: [],
+            borderDashOffset: 0.0,
+            data: [ 53, 20, 10, 80, 100, 45],
+          }]
+        },
+          options: gradientBarChartConfiguration
+      });
+
+     */
+
     drawChart(): void {
         const ctx = this.chartRef.nativeElement;
+        // let gradientStroke = ctx.getContext('2d').createLinearGradient(0,200,0,150);
+        //
+        // gradientStroke.addColorStop(1, 'rgba(29,140,248,0.2)');
+        // gradientStroke.addColorStop(0.4, 'rgba(29,140,248,0.0)');
+        // gradientStroke.addColorStop(0, 'rgba(29,140,248,0)'); //blue colors
+        //
+        // this.chartData.data.datasets[0].backgroundColor = gradientStroke;
+        // this.chartData.data.datasets[1].backgroundColor = gradientStroke;
+        // this.chartData.data.datasets[2].backgroundColor = gradientStroke;
+        // this.chartData.data.datasets[3].backgroundColor = gradientStroke;
+
         this.chart = new Chart(this.chartRef.nativeElement, this.chartData);
     }
 }

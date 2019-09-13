24f2e6be7dbb6870c995abe9cfa9c2251bb43bf6
jlhiatt <jlhiatt@ldschurch.org>
2019-09-13 13:38:25 -0600

    new past game page is done.

--- server/dbScripts/leaderchartvw.sql
+++ server/dbScripts/leaderchartvw.sql
@@ -1,7 +1,13 @@
 create or replace view leaderchartvw as
-select sch.id gameId, sch.opponent, sco.uid, sco.weekTotalScore, ca.name, sch.gameDate, (sum(sco.weekTotalScore)) totalScore
-  from schedule sch
-  join scores sco on sco.gameId = sch.id
-  join customers_auth ca on ca.uid = sco.uid
-group by sch.id, sco.uid
+select lvw.totalScore, sco.gameId, sco.weekTotalScore, ca.name, lvw.uid, sch.gameDate
+  from (
+	  select ca.uid, ca.name,  (sum(sco.weekTotalScore)) totalScore
+	    from scores sco
+	    join customers_auth ca on ca.uid = sco.uid
+	  group by sco.uid
+  ) lvw
+  join customers_auth ca on ca.uid = lvw.uid
+  join scores sco on lvw.uid = sco.uid
+  join schedule sch on sco.gameId = sch.id
+where sch.gameDate <= sysdate()
 ;
